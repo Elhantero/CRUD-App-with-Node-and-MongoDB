62645b0c70174e1cbd04aca9989df0e8caab04f7
a17-Fesenko <elhantero@gmail.com>
2018-04-09 13:13:24 +0300

    secret_info_env_file

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//load environment variables
+require('dotenv').config();
+
+
 //grab our dependencies
 const express = require('express');
 const app = express();
@@ -15,13 +19,13 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
 //connect to our database
-mongoose.connect('mongodb://footbaldb:passworddb@ds153778.mlab.com:53778/football');
+mongoose.connect(process.env.DB_URI);
 
 
 //set the routes=========================================
 app.use( require('./app/routes'));
 
-//start our server
+//start our server==========================================
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
 });
